7f4c5929cd85de601ddc8352055f19af3640ff9f
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-12-12 22:46:11 -0300

    Refactor app.ts to use RUN_SCRIPT_INTERVAL_MS and integrate logging for script execution

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { handle } from './handle'
+import { logger } from './lib/common'
 
 (() => {
-  const interval = process.env.RUN_SCRIPT_INTERVAL as string
+  const interval = process.env.RUN_SCRIPT_INTERVAL_MS as string
+
+  console.log(interval)
 
   setInterval(async () => {
     await handle()
-    console.log('run')
+      .then(() => logger.info('Script ran successfully'))
+      .catch(error => logger.error(error))
+      .finally(() => logger.info(`Next run in ${interval}ms`))
   }, +interval)
 })()
