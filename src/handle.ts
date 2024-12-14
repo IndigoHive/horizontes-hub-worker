8a7a5063fac74500ee118fd700afb1b081629304
pedroaurelli <pedroaureliano.contato@gmail.com>
2024-12-14 11:12:48 -0300

    Add schedule field to Cogfy records and update URL field references

--- src/handle.ts
+++ src/handle.ts
@@ -1,6 +1,7 @@
 import { Cogfy, TextRecordProperty } from 'cogfy'
 import { fetchAndParseSitemap, mapUrlsToCogfyCollections } from './lib/utils'
-import { cogfyCollectionNameCollectionId, cogfyCollectionNameFieldId } from './lib/constants/cogfy-collections'
+import { cogfyCollectionNameCollectionId, cogfyCollectionNameFieldScheduleId, cogfyCollectionNameFieldUrlId } from './lib/constants/cogfy-collections'
+import { addMinutes } from 'date-fns'
 
 export async function handle() {
   const urls = await fetchAndParseSitemap('https://horizontes-hub.framer.website/sitemap.xml')
@@ -10,22 +11,24 @@ export async function handle() {
     apiKey: process.env.COGFY_API_KEY
   })
 
+  const now = new Date()
+
   const collections = Object.entries(cogfyCollectionNameCollectionId)
 
   for (const collection of collections) {
     const records = await cogfy.records.query(collection[1], {
       fieldIds: [
-        cogfyCollectionNameFieldId[collection[0] as keyof typeof cogfyCollectionNameFieldId]
+        cogfyCollectionNameFieldUrlId[collection[0] as keyof typeof cogfyCollectionNameFieldUrlId]
       ]
     })
 
-    const recordValues = records.data.map(record => record.properties[cogfyCollectionNameFieldId[collection[0] as keyof typeof cogfyCollectionNameFieldId]] as TextRecordProperty)
+    const recordValues = records?.data.map(record => record.properties[cogfyCollectionNameFieldUrlId[collection[0] as keyof typeof cogfyCollectionNameFieldUrlId]] as TextRecordProperty) || []
 
     const valuesToRemove = recordValues.filter(value => value.text.value && !collectionUrls[collection[0] as keyof typeof collectionUrls].includes(value.text.value))
     const valuesToAdd = collectionUrls[collection[0] as keyof typeof collectionUrls].filter(value => !recordValues.map(recordValue => recordValue.text.value).includes(value))
 
     for (const value of valuesToRemove) {
-      const record = records.data.find(record => (record.properties[cogfyCollectionNameFieldId[collection[0] as keyof typeof cogfyCollectionNameFieldId]] as TextRecordProperty).text.value === value.text.value)
+      const record = records.data.find(record => (record.properties[cogfyCollectionNameFieldUrlId[collection[0] as keyof typeof cogfyCollectionNameFieldUrlId]] as TextRecordProperty).text.value === value.text.value)
 
       if (record) {
         await cogfy.records.delete(collection[1], record.id)
@@ -35,10 +38,21 @@ export async function handle() {
     for (const value of valuesToAdd) {
       await cogfy.records.create(collection[1], {
         properties: {
-          [cogfyCollectionNameFieldId[collection[0] as keyof typeof cogfyCollectionNameFieldId]]: {
+          [cogfyCollectionNameFieldUrlId[collection[0] as keyof typeof cogfyCollectionNameFieldUrlId]]: {
             type: 'text',
             text: {
               value
+            }
+          },
+          [cogfyCollectionNameFieldScheduleId[collection[0] as keyof typeof cogfyCollectionNameFieldScheduleId]]: {
+            type: 'schedule',
+            schedule: {
+              value: {
+                interval: 1,
+                unit: 'hours',
+                startDate: now.toISOString(),
+                nextDate: addMinutes(now.toISOString(), 5).toISOString()
+              }
             }
           }
         }
--- src/lib/constants/cogfy-collections.ts
+++ src/lib/constants/cogfy-collections.ts
@@ -16,9 +16,16 @@ export const cogfyCollectionNameCollectionId: Record<CogfyCollectionsNames, UUID
   Desafios: '92191066-b7b1-43f2-98de-739b8e16f3c6'
 }
 
-export const cogfyCollectionNameFieldId: Record<CogfyCollectionsNames, UUID> = {
+export const cogfyCollectionNameFieldUrlId: Record<CogfyCollectionsNames, UUID> = {
   Desafios: 'cba465b8-e67b-4198-a0dc-2f632c58242a',
   Empresas: 'd17e6276-c2f5-4885-8672-03528f4565b2',
   General: '5538ebc2-1028-4ddd-b5a5-88c6da213069',
   Startups: 'b2b7dfd5-6479-49ac-a73f-179ce6ccd85d'
+}
+
+export const cogfyCollectionNameFieldScheduleId: Record<CogfyCollectionsNames, UUID> = {
+  Desafios: '33688de2-ce87-45c0-b29a-b22af0575379',
+  Empresas: '25ff4f22-33cc-4792-bdea-92064c793d6b',
+  General: '6fcdfc68-ac18-4b8b-bc49-8cfb465852a3',
+  Startups: '6221ca35-7cf3-4652-b184-1e461447d783'
 }
